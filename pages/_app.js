80ddd6527f9f2ab426a04ba4f5356f5f6b742cda
Pulse founder <pulsefounder@gmail.com>
2025-06-19 20:43:49 -0400

    Fix _app.js import for build

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
 import '../styles/globals.css';
-import Layout from '../components/Layout';
-=======
-import '@/styles/globals.css'
-import Layout from '@/components/Layout'
->>>>>>> fd56ce1 (Finalize Navon UI for production)
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-<<<<<<< HEAD
-  );
-=======
-  )
->>>>>>> fd56ce1 (Finalize Navon UI for production)
+  return <Component {...pageProps} />;
 }
