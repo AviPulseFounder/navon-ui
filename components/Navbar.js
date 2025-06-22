3b98f82bb8b79161477b93f61935129e762d4682
Pulse founder <pulsefounder@gmail.com>
2025-06-22 17:54:24 -0400

    Add navigation links to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,10 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-6 py-4 bg-gray-900 text-white shadow-md">
-      <div className="text-xl font-semibold">Navon</div>
-      <div className="space-x-4">
-        <Link href="/dashboard" className="hover:underline">Dashboard</Link>
-        <Link href="/nova" className="hover:underline">Nova</Link>
-        <Link href="/axiom" className="hover:underline">Axiom</Link>
-      </div>
+    <nav className="bg-gray-900 text-white p-4 flex space-x-6">
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/nova">Nova</Link>
+      <Link href="/axiom">Axiom</Link>
     </nav>
   );
 }
