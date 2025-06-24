e32c8abb75c2b48032a0ee9194a6498bd975f733
Pulse founder <pulsefounder@gmail.com>
2025-06-24 19:43:31 -0400

    Enable Navbar links with Next.js routing

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,13 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white p-4 flex space-x-6">
-      <Link href="/dashboard">Dashboard</Link>
-      <Link href="/nova">Nova</Link>
-      <Link href="/axiom">Axiom</Link>
+    <nav className="bg-gray-800 text-white p-4">
+      <ul className="flex space-x-4">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/dashboard">Dashboard</Link></li>
+        <li><Link href="/nova">Nova</Link></li>
+        <li><Link href="/axiom">Axiom</Link></li>
+      </ul>
     </nav>
   );
 }
